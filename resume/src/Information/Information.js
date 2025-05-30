60a9c0e616d3cce93121f714af9d6f5abe1dd38d
Ankur Soni <ankursoni2974@gmail.com>
2025-05-30 12:02:35 +0530

    resolved overflow issue

--- resume/src/Information/Information.js
+++ resume/src/Information/Information.js
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
 export const Information = {email: {emailId: "ankursoni2974@gmail.com", emailIcon: <MdEmail/>},
-  phone: {phoneNumber: "+91 9305463385", phoneIcon: <IoMdPhonePortrait/>},
+  phone: {phoneNumber: "+919305463385", phoneIcon: <IoMdPhonePortrait/>},
   gitHub: {link: "https://github.com/ankursonideveloper", gitHubIcon: <FaGithub/>},
   linkedIn: {link: "https://www.linkedin.com/in/ankursoni2974", linkedInIcon: <FaLinkedin/>}
 };
