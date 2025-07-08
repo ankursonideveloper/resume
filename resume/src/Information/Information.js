6d0af688ea3e3bd978cbca6297f9540a6bbccc24
Ankur Soni <ankur@sciometrix.com>
2025-07-08 13:00:32 +0530

    updated

--- resume/src/Information/Information.js
+++ resume/src/Information/Information.js
@@ -2,10 +2,9 @@ import { MdEmail } from "react-icons/md";
 import { IoMdPhonePortrait } from "react-icons/io";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export const Information = {
-  downloadLink: "https://drive.google.com/uc?export=download&id=142ITbDNNmGlL-urDkuanufddAz-8mTW4",
+  downloadLink: "https://drive.google.com/uc?export=download&id=1EGE8ZhEe-eU8d7IDNkyQHmlGrTUD29Oj",
   about: {
     summary:
       "Software Engineer with nearly 5 years of experience specializing in Node.js, React, PostgreSQL, AWS cloud technologies, and Salesforce. Experienced in designing and implementing robust APIs, automating complex workflows, and building scalable cloud-based solutions. Proficient in integrating third-party APIs, developing dynamic dashboards, and migrating legacy systems to serverless architectures. Skilled in Git version control, Bitbucket, and agile methodologies. Passionate about leveraging technology to streamline processes, enhance system performance, and deliver impactful business solutions.",
