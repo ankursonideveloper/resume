a0b9708468c997c858c09832e0f401e3f6e5f544
Ankur Soni <ankur@sciometrix.com>
2025-07-08 12:48:32 +0530

    updated

--- resume/src/utils/downloadReactComponent.js
+++ resume/src/utils/downloadReactComponent.js
@@ -1,13 +1,13 @@
-// const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer");
 
-// const generatePDF = async()=>{
-//   const browser = await puppeteer.launch({ headless: true });
-//   const page = await browser.newPage();
-//   await page.goto('https://ankursonidev.com/', {waitUntil: 'networkidle0'});
-//   const pdf = await page.pdf({ path: 'Ankur_Resume.pdf', format: 'A4', margin : { top: '15mm', bottom: '15mm', left: '5mm', right: '5mm' } });
+const generatePDF = async()=>{
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('https://ankursonidev.com/', {waitUntil: 'networkidle0'});
+  const pdf = await page.pdf({ path: 'Ankur_Resume_with_Moonstone_July_8_2025.pdf', format: 'A4', margin : { top: '15mm', bottom: '15mm', left: '5mm', right: '5mm' } });
  
-//   await browser.close();
-//   return pdf
-// }
+  await browser.close();
+  return pdf
+}
 
-// generatePDF();
+generatePDF();
