371bb73c70c3ae25ee3b5f79da33634f8c550f6d
Ankur Soni <ankur@sciometrix.com>
2025-07-08 23:12:29 +0530

    updated

--- resume/src/utils/downloadReactComponent.js
+++ resume/src/utils/downloadReactComponent.js
@@ -1,13 +1,30 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-const generatePDF = async()=>{
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto('https://ankursonidev.com/', {waitUntil: 'networkidle0'});
-  const pdf = await page.pdf({ path: 'Ankur_Resume_withOut_Moonstone_July_8_2025.pdf', format: 'A4', margin : { top: '18mm', bottom: '18mm' } });
- 
+(async () => {
+  const browser = await puppeteer.launch();
+  const page    = await browser.newPage();
+  await page.goto('https://ankursonidev.com/', { waitUntil: 'networkidle0' });
+
+  // override for print layout
+  await page.addStyleTag({
+    content: `
+      @media print {
+        .Resume {
+          width: 90% !important;
+          max-width: none !important;
+          margin-left: 13mm,
+          margin-right: 13mm,
+          padding: 0 18mm 0 !important;
+        }
+      }
+    `
+  });
+
+  await page.pdf({
+    path: 'Ankur_Resume_with_moonstone_July_8_2025.pdf',
+    format: 'A4',
+    margin: { top: '10mm', bottom: '10mm', left: '0mm', right: '0mm' }
+  });
+
   await browser.close();
-  return pdf
-}
-
-generatePDF();
+})();
